98182a9d7703c63731051deb979eed49f0c89231
Michael Bond <michael@bondcodes.com>
2025-07-18 11:42:56 -0400

    Add project files.

---
+++ ConsoleAITest/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.AI;
+
+IChatClient chatClient = new OllamaSharp.OllamaApiClient(
+        new Uri("http://127.0.0.1:11434"), "llama3.1");
+
+var prompt = Console.ReadLine();
+
+var response = await chatClient.GetResponseAsync(prompt);
+
+Console.WriteLine(response);
